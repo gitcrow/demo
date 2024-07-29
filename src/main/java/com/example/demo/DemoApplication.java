8d03c29bc4df44688437f1c70cfa10c9aec0dfa3
최영민 <cym@dodoom.co.kr>
2024-07-29 17:26:28 +0900

    modified:   src/main/java/com/example/demo/DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,34 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("helloo~~");
 	}
 
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name){
+		this(UUID.randomUUID().toString(), name); //이름만 넣고 클래스 생성할 경우 랜덤 id 부여
+	}
+
+	public String getId(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
 }
